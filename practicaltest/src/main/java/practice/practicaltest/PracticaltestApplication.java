83c897b700e7f91dec5244711201fb8b945f7043
이정주 <mozylee@daou.co.kr>
2023-10-11 10:48:08 +0900

    init project settings

---
+++ practicaltest/src/main/java/practice/practicaltest/PracticaltestApplication.java
@@ -0,0 +1,13 @@
+package practice.practicaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaltestApplication.class, args);
+    }
+
+}
