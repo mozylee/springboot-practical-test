bdbfade3499a1e6bf96155de1c3802d8440da548
이정주 <whyalwaysmeyy@gmail.com>
2023-10-18 18:06:46 +0900

    chore: JPA 관련 설정 변경 (#SECTION-05)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,2 @@
-insert into product(product_number, product_type, product_selling_type, name, price)
-            values ('001', 'HANDMADE', 'SELLING', '아메리카노', 4000),
-                   ('002', 'HANDMADE', 'HOLD', '라떼', 4500),
-                   ('003', 'BAKERY', 'STOP_SELLING', '크루아상', 3500);
+insert into stock(product_number, quantity)
+values ('999', 2);
