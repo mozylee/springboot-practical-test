28f96f7acfc1e6607f94e58235f8df3c39c32665
이정주 <whyalwaysmeyy@gmail.com>
2023-10-18 14:44:57 +0900

    feat: 주문 생성 서비스 및 테스트 코드 개발 (#SECTION-05)

--- src/main/java/practice/practicaltest/spring/api/service/order/OrderService.java
+++ src/main/java/practice/practicaltest/spring/api/service/order/OrderService.java
@@ -1,5 +1,6 @@
 package practice.practicaltest.spring.api.service.order;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,9 +11,11 @@ import practice.practicaltest.spring.api.service.order.dto.OrderCreateRequest;
 import practice.practicaltest.spring.api.service.order.dto.OrderCreateResponse;
 import practice.practicaltest.spring.domain.order.Order;
 import practice.practicaltest.spring.domain.order.OrderRepository;
-import practice.practicaltest.spring.domain.orderproduct.OrderProduct;
 import practice.practicaltest.spring.domain.product.Product;
 import practice.practicaltest.spring.domain.product.ProductRepository;
+import practice.practicaltest.spring.domain.product.ProductType;
+import practice.practicaltest.spring.domain.stock.Stock;
+import practice.practicaltest.spring.domain.stock.StockRepository;
 
 @RequiredArgsConstructor
 @Service
@@ -22,17 +25,19 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
+    private final StockRepository stockRepository;
+
     @Transactional
     public OrderCreateResponse createOrder(OrderCreateRequest request) {
-        List<Product> duplicatedProducts = findProductsBy(request);
-        Order order = Order.create(duplicatedProducts,
-                                   request.getOrderDateTime());
+        List<Product> products = findProductsBy(request.getProductNumbers());
+        deductStockQuantities(products);
+
+        Order order = Order.create(products, request.getOrderDateTime());
 
         return OrderCreateResponse.of(orderRepository.save(order));
     }
 
-    private List<Product> findProductsBy(OrderCreateRequest request) {
-        List<String> productNumbers = request.getProductNumbers();
+    private List<Product> findProductsBy(List<String> productNumbers) {
         List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
 
         // 중복 상품 처리
@@ -44,6 +49,44 @@ public class OrderService {
                                                          .toList();
 
         return duplicatedProducts;
+    }
+
+    private void deductStockQuantities(List<Product> products) {
+        List<String> stockProductNumbers = extractStockProductNumbers(products);
+
+        List<Stock> stocks = stockRepository.findAllByProductNumberIn(stockProductNumbers);
+
+        Map<String, Stock> productStockMap = createProductStockMapBy(stocks);
+        Map<String, Long> requiredQuantityMap = createRequiredQuantityMapBy(stockProductNumbers);
+
+        for (String stockProductNumber : new HashSet<>(stockProductNumbers)) {
+            Stock stock = productStockMap.get(stockProductNumber);
+            int requiredQuantity = requiredQuantityMap.get(stockProductNumber).intValue();
+
+            if (stock.isQuantityLessThan(requiredQuantity)) {
+                throw new IllegalArgumentException("재고가 부족한 상품이 있습니다.");
+            }
+
+            stock.deductQuantity(requiredQuantity);
+        }
+    }
+
+    private static List<String> extractStockProductNumbers(List<Product> products) {
+        List<String> stockProductNumbers = products.stream()
+                                                   .filter(product -> ProductType.containsStockType(product.getProductType()))
+                                                   .map(Product::getProductNumber)
+                                                   .toList();
+        return stockProductNumbers;
+    }
+
+    private static Map<String, Stock> createProductStockMapBy(List<Stock> stocks) {
+        return stocks.stream()
+                     .collect(Collectors.toMap(Stock::getProductNumber, p -> p));
+    }
+
+    private static Map<String, Long> createRequiredQuantityMapBy(List<String> stockProductNumbers) {
+        return stockProductNumbers.stream()
+                                  .collect(Collectors.groupingBy(p -> p, Collectors.counting()));
     }
 
 }
